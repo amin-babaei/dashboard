d610b4d45e5d16c514837fff9e7555bfa2ca1138
amin <aminbabaei82@gmail.com>
2023-03-08 21:38:15 +0330

    added react-lazy-load-image-component

--- src/pages/customers/Customers.tsx
+++ src/pages/customers/Customers.tsx
@@ -3,8 +3,9 @@ import { useQuery } from "@apollo/client";
 import { Box, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import GET_CHARACTERS from "../../gql/customers";
 import { CharactersResponse } from "./customers-interface";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import Loading from './Loading';
-
+import 'react-lazy-load-image-component/src/effects/blur.css';
 const Customers = () => {
   const [page, setPage] = useState(1);
   const scollToRef = useRef<HTMLDivElement>(null);
@@ -19,44 +20,44 @@ const Customers = () => {
     scollToRef.current?.scrollIntoView({ behavior: 'smooth' })
   };
 
-  if(loading)return <Loading/>
-  if(error)return <Typography>{error.message}</Typography>
+  if (loading) return <Loading />
+  if (error) return <Typography>{error.message}</Typography>
   return (
     <TableContainer component={Paper} ref={scollToRef}>
-        <Box component='div' sx={{ overflow: "auto" }}>
-          <Box component='div' sx={{ width: "100%", display: 'table', tableLayout: "fixed" }}>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">image</TableCell>
-                  <TableCell align="center">name</TableCell>
-                  <TableCell align="center">gender</TableCell>
-                  <TableCell align="center">status</TableCell>
-                  <TableCell align="center">id</TableCell>
+      <Box component='div' sx={{ overflow: "auto" }}>
+        <Box component='div' sx={{ width: "100%", display: 'table', tableLayout: "fixed" }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">image</TableCell>
+                <TableCell align="center">name</TableCell>
+                <TableCell align="center">gender</TableCell>
+                <TableCell align="center">status</TableCell>
+                <TableCell align="center">id</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {item?.map(cararcter => (
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  key={cararcter.id}
+                >
+                  <TableCell align="left">
+                    <LazyLoadImage src={cararcter.image} width={100} alt={cararcter.name} effect='blur'/>
+                  </TableCell>
+                  <TableCell align="center">
+                    {cararcter.name}
+                  </TableCell>
+                  <TableCell align="center">{cararcter.gender}</TableCell>
+                  <TableCell align="center">{cararcter.status}</TableCell>
+                  <TableCell align="center">{cararcter.id}</TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {item?.map(cararcter => (
-                  <TableRow
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    key={cararcter.id}
-                  >
-                    <TableCell align="left">
-                      <img src={cararcter.image} alt="" width={100} />
-                    </TableCell>
-                    <TableCell align="center">
-                      {cararcter.name}
-                    </TableCell>
-                    <TableCell align="center">{cararcter.gender}</TableCell>
-                    <TableCell align="center">{cararcter.status}</TableCell>
-                    <TableCell align="center">{cararcter.id}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-              <Pagination sx={{width:'500px'}} count={info?.pages} page={page} onChange={handleChange} />
-            </Table>
-          </Box>
+              ))}
+            </TableBody>
+          </Table>
+          <Pagination sx={{ width: '500px' }} count={info?.pages} page={page} onChange={handleChange} />
         </Box>
+      </Box>
     </TableContainer>
   );
 };
