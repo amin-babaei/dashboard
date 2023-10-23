571a8820bc68e63a01967c59cd285ee17d22cd9f
amin <aminbabaei82@gmail.com>
2023-10-23 20:38:33 +0330

    feat: add sound volume control

--- src/components/audio/Player.tsx
+++ src/components/audio/Player.tsx
@@ -1,14 +1,16 @@
-import { Box, Button, Slider } from "@mui/material";
+import { Box, Button, Slider, Stack } from "@mui/material";
 import { useRef, useState } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
 import { Spectrum } from "./Spectrum";
+import { VolumeDown, VolumeUp } from "@mui/icons-material";
 
 export function Player({ audioUrl }: { audioUrl: string }) {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPaused, setIsPaused] = useState(false);
   const [isStopped, setIsStopped] = useState(true);
   const [currentTime, setCurrentTime] = useState<number>(0);
+  const [volume, setVolume] = useState<number>(20);
 
   const handleSliderChange = (event: any, newValue: number | number[]) => {
     if (typeof newValue === 'number') {
@@ -20,6 +22,16 @@ export function Player({ audioUrl }: { audioUrl: string }) {
   const handleTimeUpdate = () => {
     setCurrentTime(audioRef.current!.currentTime);
   }
+
+  const handleVolumeChange = (event: React.ChangeEvent<{}>, newValue: number | number[]) => {
+  if (typeof newValue === 'number') {
+    const newVolume = newValue as number;
+    setVolume(newVolume);
+    if (audioRef.current) {
+      audioRef.current.volume = newVolume / 100;
+    }
+  }
+};
 
   const handleEnded = () => {
     setIsPaused(true);
@@ -62,24 +74,47 @@ export function Player({ audioUrl }: { audioUrl: string }) {
       <audio key={audioUrl} id={audioUrl} ref={audioRef} onTimeUpdate={handleTimeUpdate} onEnded={handleEnded}>
         <source src={audioUrl} />
       </audio>
-      <Slider
-        min={0}
-        max={audioRef.current ? audioRef.current.duration : 0}
-        value={currentTime}
-        onChange={handleSliderChange}
-        sx={{
-          width: '85%',
-          margin: 'auto',
-          display: 'block',
-          color: '#ed6c02',
-          mb: 1,
-          '& .MuiSlider-thumb': {
-            '&:hover': {
-              boxShadow: '0px 0px 0px 4px rgba(254, 186, 67, 0.5)'
+      <Stack justifyContent="space-between" alignItems="flex-start" sx={{ mb: 1, flexDirection: { xs: 'column', md:'row' }}} ml={1} gap={2}>
+        <Slider
+          min={0}
+          max={audioRef.current ? audioRef.current.duration : 0}
+          value={currentTime}
+          onChange={handleSliderChange}
+          sx={{
+            width: { xs:'100%', md:'80%' },
+            display: 'block',
+            color: '#ed6c02',
+            mb: 1,
+            ml: 1,
+            '& .MuiSlider-thumb': {
+              '&:hover': {
+                boxShadow: '0px 0px 0px 4px rgba(254, 186, 67, 0.5)'
+              },
             },
-          },
-        }}
-      />
+          }}
+        />
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center" width={{xs:'100%',md:'20%'}} mt={0}>
+          <VolumeDown />
+          <Slider
+            min={0}
+            max={100}
+            value={volume}
+            onChange={(event: any, newValue: number | number[]) => handleVolumeChange(event, newValue)}
+            sx={{
+              width: '100%',
+              display: 'block',
+              color: '#ed6c02',
+              mb: 1,
+              '& .MuiSlider-thumb': {
+                '&:hover': {
+                  boxShadow: '0px 0px 0px 4px rgba(254, 186, 67, 0.5)'
+                },
+              },
+            }}
+          />
+          <VolumeUp />
+        </Stack>
+      </Stack>
     </Box>
   );
 }
